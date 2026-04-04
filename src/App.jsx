7ca285801d59b7d5c9bd04ab26f0ad6e182b74dd
aosheba5-glitch <aosheba5@gmail.com>
2026-04-04 23:49:41 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -406,7 +406,7 @@ function App() {
                 <span>{t.crIssueDate}</span>
                 <strong>
                   <DatePair
-                    hijriDate="07-11-1432"
+                    hijriDate="1432-11-07"
                     gregDate="2011-10-05"
                     dir={t.dir}
                   />
@@ -417,7 +417,7 @@ function App() {
                 <span>{t.annualConfirmation}</span>
                 <strong>
                   <DatePair
-                    hijriDate="24-09-1448"
+                    hijriDate="1448-09-24"
                     gregDate="2027-03-03"
                     dir={t.dir}
                   />
